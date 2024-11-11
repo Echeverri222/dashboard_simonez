1465ac72fcec4512d70f28902b52270bf78a4fe5
Simón Echeverri <simon.echeverri2003@gmail.com>
2024-11-11 10:59:06 -0500

    Update dashboard_general.py

--- proyecto_streamlit/dashboard_general.py
+++ proyecto_streamlit/dashboard_general.py
@@ -1,9 +1,9 @@
-import streamlit as st
-import plotly.express as px
-import plotly.graph_objects as go
-import pandas as pd
-import yfinance as yf
-import numpy as np
+import streamlit as st # type: ignore
+import plotly.express as px # type: ignore
+import plotly.graph_objects as go # type: ignore
+import pandas as pd # type: ignore
+import yfinance as yf # type: ignore
+import numpy as np # type: ignore
 
 # Set page config
 st.set_page_config(
